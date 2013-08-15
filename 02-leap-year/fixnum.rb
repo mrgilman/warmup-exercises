7d53485380a67bfd30c8d19a9dc64b9355c39e23
Melanie Gilman <melanie.gilman@livingsocial.com>
2013-08-15 18:32:13 -0400

    Monkeypatch fixnum

--- 02-leap-year/fixnum.rb
+++ 02-leap-year/fixnum.rb
@@ -1,5 +1,22 @@
 class Fixnum
 
+  def leap_year?
+    divisible_by_400? || (divisible_by_4? && !divisible_by_100?)
+  end
+
+  private
+
+  def divisible_by_4?
+    self % 4 == 0
+  end
+
+  def divisible_by_100?
+    self % 100 == 0
+  end
+
+  def divisible_by_400?
+    self % 400 == 0
+  end
 
 end
 
